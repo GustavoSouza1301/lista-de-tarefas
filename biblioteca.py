eab828887aeadcb00c34516f2e3f39c0ef2ea8cb
Gustavo <gggustavo1301@gmail.com>
2025-05-14 22:49:07 -0300

    Novo cod, com add de def

--- biblioteca.py
+++ biblioteca.py
@@ -1,20 +1,46 @@
-"""def listaTarefas(lista):
-    continuar = input("Deseja adicionar uma tarefa hoje? (s/n): ")
+def menuLista(tarefa):
+    while True:
+        menu = ("\nMENU\n"
+                "1 - Mostrar Tarefas\n"
+                "2 - Inserir Tarefa\n"
+                "3 - Deletar Tarefa\n"
+                "4 - Sair")
+        print(menu)
+        perguntaTarefa = input("Escolha uma opção: ")
+        opcao = perguntaTarefa
 
-    for i in range(len(lista)):
-        if continuar == "s":
-            lista[i] = input("Digite sua tarefa: ")
-            continuar = input("Deseja adicionar mais tarefa? (s/n):  ")
-        else:
+        adicionarLista(tarefa, opcao)
+        mostraLista(tarefa, opcao)
+        deletarLista(tarefa, opcao)
+        print("-"*60)
+        if perguntaTarefa == "4":
             break
-            print("-") * 60
-    print()
-    if lista != " ":
-        print("Suas tarefas do dia são: ")
-        for x in lista:
-            print("-", x)"""
 
-lista = [1,2,3]
-for i in range(len(lista)):
+def adicionarLista (tarefa, opcao):
+    if opcao == "2":
+        tarefa.append(input("Qual a sua tarefa ? "))
 
-    print(len(i))
+def mostraLista (tarefa, opcao):
+    if opcao == "1":
+        if len (tarefa) == 0:
+            print("A lista está vazia")
+        else:
+            for i in tarefa:
+                print(f"- {i}")
+
+def deletarLista (tarefa, opcao):
+    if opcao == "3":
+        if not tarefa:
+            print("A lista está vazia")
+        else:
+            for x in range(len(tarefa)):
+                print(f"- {tarefa[x]}: {x}")
+            pergunta = int(input("Digite o número da tarefa que deseja deletar: "))
+            tarefa.pop(pergunta)
+            print("Tareda deleta com sucesso")
+
+        """for x in range(len(tarefa)):
+                        print(f"- {tarefa[x]}: {x}")
+                        break
+                tarefa.pop
+                print("Tarefas deletadas com sucesso ")"""
--- lista.py
+++ lista.py
@@ -1,40 +1,8 @@
-lista = [" "]*5
-adicionar = " "
-apagar = " "
-continuar = input("Deseja adicionar uma tarefa hoje? (s/n): ")
-print("-" * 60)
+from biblioteca import *
+tarefa = [0,1,2,3,4,5]
 
-for i in range(len(lista)):
-    if continuar == "s":
-        lista[i] = input("Digite sua tarefa: ")
-        continuar = input("Deseja adicionar mais tarefa? (s/n):  ")
-        print("-"*60)
-    else:
-        break
-        print("-"*60)
-print()
-for x in lista:
-    if x != " ":
-        print("Suas tarefas do dia são: ")
-        print("-",x)
-print()
-adicionar = input("Deseja adicionar uma nova tarefa na lista? (s/n): ")
-
-while adicionar == "s" or adicionar == "S":
-    tarefaNova = input("Digite sua tarefa: ")
-    lista.append(tarefaNova)
-    for x in lista:
-        if x != " ":
-            print("Suas tarefas do dia são: ")
-            print("-", x)
-    adicionar = input("Deseja adicionar uma nova tarefa na lista? (s/n): ")
-    print()
-else:
-    apagar = input("Deseja excluir algumas tarefa? (s/n): ")
-    if apagar == "s" or apagar == "S":
-        for x in lista:
-            if x != " ":
-                print(f"- {x} \n"
-                      f"número da tarefa {cont}")
-        print("-" * 60)
+menuLista(tarefa)
+adicionarLista(tarefa, 0)
+mostraLista(tarefa, 0)
+deletarLista(tarefa, 0)
 
